7b4384b06cbaf13d9cf62711ae99cbaa877e294d
Stephanie Song <songstephanie03@gmail.com>
2016-08-11 13:27:24 +0000

    a working seeding sol incorporating WB data and polity data

--- model.py
+++ model.py
@@ -4,27 +4,28 @@ db = SQLAlchemy()
 class Country(db.Model):
     __tablename__ = "countries"
 
-    country_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    #country_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    country_name = db.Column(db.String(50), primary_key=True)
     country_code = db.Column(db.String(10), nullable=True)
-    country_name = db.Column(db.String(50), nullable=False)
-    #uncomment out indicator after you create indicator obj properly
     #indicators = db.relationship("Indicator")
 
     def __repr__(self):
-        return "<Country country_id=%s country_code=%s country_name=%s>" % (self.country_id, self.country_code, self.country_name)
+        return "<Country country_code=%s country_name=%s> \n" % (self.country_code, self.country_name)
 
 
 class Indicators(db.Model):
     __tablename__ = "indicators"
 
     indicator_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    country_id = db.Column(db.Integer, db.ForeignKey('countries.country_id'))
+    #country_id = db.Column(db.Integer, db.ForeignKey('countries.country_id'))
+    country_name = db.Column(db.String(50), db.ForeignKey('countries.country_name'))
     year = db.Column(db.Integer, nullable=False)
     polity = db.Column(db.Integer, nullable=True)
-    gini = db.Column(db.Integer, nullable=True)
+    gdp_per_cap = db.Column(db.Integer, nullable=True)
+    country = db.relationship('Country', backref=db.backref("indicators", order_by=year))
 
     def __repr__(self):
-        return "<Indicators indicator_id=%s country_name=%s year=%s polity=%s gini=%s>" % (self.indicator_id, self.country_name, self.year, self.polity, self.gini)
+        return "<Indicators indicator_id=%s country_name=%s year=%s polity=%s gdp_per_cap=%s> \n" % (self.indicator_id, self.country_name, self.year, self.polity, self.gdp_per_cap)
 
 
 ###
--- seed.py
+++ seed.py
@@ -1,4 +1,5 @@
 import csv
+import requests
 from model import Country, Indicators, connect_to_db, db
 from server import app
 
@@ -14,16 +15,60 @@ def load_polity_scores():
     for row in csv_f:
         country_code = row[1]
         country_name = row[2]
-        year = row[3]
+        year = int(row[3])
         polity_score = row[4]
-        #test code
-        country = Country(country_code=country_code, country_name=country_name)
+
+        # some have no polity score values (empty str with no spaces) thus we need to do this otherwise we'll get invalid literal error
+        if polity_score is not '':
+            polity_score = int(polity_score)
+        else:
+            polity_score = None
+
+        # create country obj if it hasn't been created already
+        # seems to work ok
+        check = Country.query.filter_by(country_name=country_name).all()
+        #create the country first
+        if check == []:
+            country = Country(country_code=country_code, country_name=country_name)
+
+        indicators = Indicators(country_name=country_name, year=year, polity=polity_score)
+
         db.session.add(country)
+        db.session.add(indicators)
 
     db.session.commit()
+
+def add_gdp():
+    r = requests.get('http://api.worldbank.org/countries/all/indicators/NY.GDP.PCAP.CD?per_page=14784&format=json')
+    gdp_per_cap = r.json()
+    #list with dicts containing gdp info
+    gdp_per_cap_list = gdp_per_cap[1]
+
+    for i in gdp_per_cap_list:
+        country_name = i["country"]["value"]
+        year = int(i["date"])
+
+        if i["value"] is not None:
+            value = float(i["value"])
+        else:
+            value = None
+
+        #.first() gets first record, returns none if none found so you can do an if statement
+        fetch = Indicators.query.filter(Indicators.country_name == country_name, Indicators.year == year).first()
+
+        if fetch:
+            fetch.gdp_per_cap = value
+
+    db.session.commit()
+
+
+
+
+
 
 if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
 
     load_polity_scores()
+    add_gdp()
